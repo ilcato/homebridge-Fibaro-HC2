aefb9e4f85953ecf0e5d618250887a6bcf4af270
ilcato <andrea.catozzi@gmail.com>
2018-03-12 11:01:51 +0100

    Managed Fibaro Thermostat

--- src/shadows.ts
+++ src/shadows.ts
@@ -190,6 +190,7 @@ export class ShadowAccessory {
 				ss = [new ShadowService(new hapService.LockMechanism(device.name), [hapCharacteristic.LockCurrentState, hapCharacteristic.LockTargetState])];
 				break;
 			case "com.fibaro.setPoint":
+			case "com.fibaro.FGT001":
 			case "com.fibaro.thermostatDanfoss":
 			case "com.fibaro.com.fibaro.thermostatHorstmann":
 				ss = [new ShadowService(new hapService.Thermostat(device.name), [hapCharacteristic.CurrentTemperature, hapCharacteristic.TargetTemperature, hapCharacteristic.CurrentHeatingCoolingState, hapCharacteristic.TargetHeatingCoolingState, hapCharacteristic.TemperatureDisplayUnits])];
